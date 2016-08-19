5be1d349ced464e598850d0f5c8e7dee249af56e
Nathan Smith <nathan.smith5@mail.mcgil.ca>
2016-08-19 00:54:49 -0400

    Add simple test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,4 @@ using Maxima
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
-
-# Text that incorrect expr "1+" does not hang
-@test string(mcall(m"1+")) == "incorrectsyntax:Prematureterminationofinputat;.1+;^"
-
+@test m"trigsimp(sin(x)^2 + cos(x)^2)" |> mcall |> parse |> eval == 1
