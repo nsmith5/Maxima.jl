5ec742c1ab31661b46131c172d5d642ca26df25a
Michael Reed <michaelreed@crucialflow.com>
2017-05-10 17:16:46 -0400

    added precompile

--- src/setup.jl
+++ src/setup.jl
@@ -1,25 +1,29 @@
 # 	This file is part of Maxima.jl. It is licensed under the MIT license
 #   Copyright (c) 2016 Nathan Smith
 
-try
-	if is_unix()
-		@compat	readstring(`maxima --version`)
-	else
-		@compat readstring(`maxima.bat --version`)
+ResetMaxima() = (kill(ms); LoadMaxima())
+__init__() = (LoadMaxima(); atexit(() -> kill(ms)))
+
+function LoadMaxima()
+	try
+		if is_unix()
+			@compat	readstring(`maxima --version`)
+		else
+			@compat readstring(`maxima.bat --version`)
+		end
+	catch err
+		error("Looks like Maxima is either not installed or not in the path")
 	end
-catch err
-	error("Looks like Maxima is either not installed or not in the path")
-end
 
-# Server setup
+	# Server setup
 
-const ms = MaximaSession()	# Spin up a Maxima session
-atexit(() -> kill(ms))  	# Kill the session on exit
+	global ms = MaximaSession()	# Spin up a Maxima session
 
-# REPL setup
-repl_active = isdefined(Base, :active_repl)	# Is an active repl defined?
-interactive = isinteractive()				# In interactive mode?
+	# REPL setup
+	repl_active = isdefined(Base, :active_repl)	# Is an active repl defined?
+	interactive = isinteractive()				# In interactive mode?
 
-if repl_active && interactive
-	repl_init(Base.active_repl)
+	if repl_active && interactive
+		repl_init(Base.active_repl)
+	end
 end
