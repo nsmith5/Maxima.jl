ce62218dffa3940c6255309007bea8980401fd5c
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2017-05-10 18:06:40 -0400

    Formatting changes
    * 4 spaces instead of tabs
    * use triple statement instead of if/else

--- src/setup.jl
+++ src/setup.jl
@@ -1,29 +1,26 @@
-# 	This file is part of Maxima.jl. It is licensed under the MIT license
+#   This file is part of Maxima.jl. It is licensed under the MIT license
 #   Copyright (c) 2016 Nathan Smith
 
 ResetMaxima() = (kill(ms); LoadMaxima())
 __init__() = (LoadMaxima(); atexit(() -> kill(ms)))
 
 function LoadMaxima()
-	try
-		if is_unix()
-			@compat	readstring(`maxima --version`)
-		else
-			@compat readstring(`maxima.bat --version`)
-		end
-	catch err
-		error("Looks like Maxima is either not installed or not in the path")
-	end
+    try
+	    is_unix() ? @compat	readstring(`maxima --version`) : 
+            @compat readstring(`maxima.bat --version`)
+    catch err
+        error("Looks like Maxima is either not installed or not in the path")
+    end
 
 	# Server setup
 
-	global ms = MaximaSession()	# Spin up a Maxima session
+    global ms = MaximaSession()	# Spin up a Maxima session
 
 	# REPL setup
-	repl_active = isdefined(Base, :active_repl)	# Is an active repl defined?
-	interactive = isinteractive()				# In interactive mode?
+    repl_active = isdefined(Base, :active_repl)	# Is an active repl defined?
+    interactive = isinteractive()				# In interactive mode?
 
-	if repl_active && interactive
-		repl_init(Base.active_repl)
-	end
+    if repl_active && interactive
+	    repl_init(Base.active_repl)
+    end
 end
