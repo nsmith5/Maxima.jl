43b421c569769c15c3dee2a39f7c41c3a1e2ba3c
Michael Reed <michaelreed@crucialflow.com>
2017-06-08 22:16:31 -0400

    simplify code generation

--- src/simplify.jl
+++ src/simplify.jl
@@ -1,27 +1,8 @@
 #   This file is part of Maxima.jl. It is licensed under the MIT license
 #   Copyright (c) 2016 Nathan Smith
 
-export float,
-       subst,
-       ratsimp,
-       radcan,
-       factor,
-       gfactor,
-       expand,
-       logcontract,
-       logexpand,
-       makefact,
-       makegamma,
-       trigsimp,
-       trigreduce,
-       trigexpand,
-       trigrat,
-       rectform,
-       polarform,
-       realpart,
-       imagpart,
-       demoivre,
-       exponentialize
+export subst,
+       logexpand
 
 import Base: expand,
              diff,
@@ -31,7 +12,44 @@ if VERSION < v"0.6-"
     import Base: factor
 end
 
-"""
+simfuns = [
+  :ratsimp,:radcan,
+  :factor,:gfactor,:expand,
+  :logcontract,
+  :makefact,:makegamma,
+  :trigsimp,:trigreduce,:trigexpand,:trigrat,
+  :rectform,:polarform,
+  :realpart,:imagpart,
+  :demoivre,
+  :exponentialize
+  :float]
+
+:(export $(simfuns...)) |> eval
+
+for i ∈ simfuns
+  for t ∈ [:(Compat.String),:Expr]
+    quote
+      function $i(expr::$t)
+        mexpr = MExpr(expr)
+        out = mcall(MExpr($(string(i))*"($mexpr)"))
+        convert($t, out)
+      end
+    end |> eval
+  end
+  quote
+    function $i(expr::MExpr)
+      mcall(MExpr($(string(i))*"($expr)"))
+    end
+  end |> eval
+end
+
+#function $i{T}(expr::T)
+#  mexpr = MExpr(expr)
+#  out = mcall(MExpr($(string(i))*"($mexpr)"))
+#  convert(T, out)
+#end
+
+@doc """
     ratsimp{T}(expr::T)
 
 Simplify expression.
@@ -46,17 +64,12 @@ julia> ratsimp(:(sin(asin(a + b/c))))
 :((a * c + b) / c)
 
 julia> ratsimp(m\"%e^log(x)\")
- 
+
                                        x
 ```
-"""
-function ratsimp{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("ratsimp($mexpr)"))
-    convert(T, out)
-end
+""" ratsimp
 
-"""
+@doc """
     radcan{T}(expr::T)
 
 Simplify radicals in expression.
@@ -67,21 +80,15 @@ julia> radcan(:(sqrt(x/y)))
 :(sqrt(x)/sqrt(y))
 
 julia> radcan(m\"sqrt(x/y)\")
- 
+
                                     sqrt(x)
                                     -------
                                     sqrt(y)
 
 ```
-"""
-function radcan{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("radcan($mexpr)"))
-    convert(T, out)
-end
+""" radcan
 
-
-"""
+@doc """
     factor{T}(expr::T)
 
 Factorize polynomial expression
@@ -93,18 +100,13 @@ julia> factor(:(x^2 + 2x + 1))
 :((x + 1) ^ 2)
 
 julia> factor(MExpr(\"a^2 - b^2\"))
- 
+
                                 (a - b) (b + a)
 
 ```
-"""
-function factor{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("factor($mexpr)"))
-    convert(T, out)
-end
+""" factor
 
-"""
+@doc """
     gfactor{T}(expr::T)
 
 Factorize complex polynomial expression
@@ -116,53 +118,28 @@ julia> gfactor(:(z^2 + 2*im*z - 1))
 :((z + im) ^ 2)
 
 ```
-"""
-function gfactor{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("gfactor($mexpr)"))
-    convert(T, out)
-end
+""" gfactor
 
 """
     expand(expr)
 
-Expand expression. 
+Expand expression.
 
 ## Examples
 ```julia
 julia> expand(m\"(a + b)^2\")
- 
+
                                  2            2
                                 b  + 2 a b + a
 
 ```
-"""
-function expand(expr::Compat.String)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("expand($mexpr)"))
-    convert(Compat.String, out)
-end
+""" expand
 
-function expand(expr::Expr)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("expand($mexpr)"))
-    convert(Expr, out)
-end
-
-function expand(expr::MExpr)
-    mcall(MExpr("expand($expr)"))
-end
-
-"""
+@doc """
     logcontract{T}(expr::T)
 
 Contract logarithms in expression
-"""
-function logcontract{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("logcontract($mexpr)"))
-    convert(T, out)
-end
+""" logcontract
 
 """
     logexpand{T}(expr::T)
@@ -175,29 +152,19 @@ function logexpand{T}(expr::T)
     convert(T, out)
 end
 
-"""
+@doc """
     makefact{T}(expr::T)
 
 Convert expression into factorial form.
-"""
-function makefact{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("makefact($mexpr)"))
-    convert(T, out)
-end
+""" makefact
 
-"""
+@doc """
     makegamma{T}(expr::T)
 
 Convert factorial to gamma functions in expression
-"""
-function makegamma{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("makegamma($mexpr)"))
-    convert(T, out)
-end
+""" makegamma
 
-"""
+@doc """
     trigsimp{T}(expr::T)
 
 Simplify trigonometric expression
@@ -205,39 +172,24 @@ Simplify trigonometric expression
 ## Examples
 ```julia
 julia> trigsimp(m\"sin(x)^2 + cos(x)^2\")
- 
+
                                        1
 ```
-"""
-function trigsimp{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("trigsimp($mexpr)"))
-    convert(T, out)
-end
+""" trigsimp
 
-"""
+@doc """
     trigreduce(expr)
 
 Contract trigonometric functions
-"""
-function trigreduce{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("trigreduce($mexpr)"))
-    convert(T, out)
-end
+""" trigreduce
 
-"""
+@doc """
     trigexpand(expr)
 
 Expand out trig functions in expression
-"""
-function trigexpand{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("trigexpand($mexpr)"))
-    convert(T, out)
-end
+""" trigexpand
 
-"""
+@doc """
     trigrat(expr)
 
 Convert expression into a canonical trigonometric form
@@ -248,14 +200,9 @@ julia> trigrat(:(exp(im*x) + exp(-im*x)))
 :(2 * cos(x))
 
 ```
-"""
-function trigrat{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("trigrat($mexpr)"))
-    convert(T, out)
-end
+""" trigrat
 
-"""
+@doc """
     rectform(expr)
 
 Put complex expression in rectangular form
@@ -267,14 +214,9 @@ julia> rectform(:(R*e^(im*θ)))
 
 
 ```
-"""
-function rectform{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("rectform($mexpr)"))
-    convert(T, out)
-end
+""" rectform
 
-"""
+@doc """
     polarform(expr)
 
 Put a complex expression into polarform
@@ -282,19 +224,14 @@ Put a complex expression into polarform
 ## Examples
 ```julia
 julia> polarform(m\"a + %i*b\")
- 
+
                               2    2    %i atan2(b, a)
                         sqrt(b  + a ) %e
 
 ```
-"""
-function polarform{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("polarform($mexpr)"))
-    convert(T, out)
-end
+""" polarform
 
-"""
+@doc """
     realpart(expr)
 
 Find the real part of a complex expression
@@ -306,15 +243,9 @@ julia> realpart(MExpr(\"a + %i*b\"))
                             a
 
 ```
-"""
-function realpart{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("realpart($mexpr)"))
-    convert(T, out)
-end
+""" realpart
 
-
-"""
+@doc """
     imagpart(expr)
 
 Find the imaginary part of a complex expression.
@@ -326,47 +257,37 @@ julia> realpart(MExpr(\"a + %i*b\"))
                             b
 
 ```
-"""
-function imagpart{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("imagpart($mexpr)"))
-    convert(T, out)
-end
+""" imagpart
 
-"""
+@doc """
     demoivre(expr)
 
-Break exponential terms into hyperbolic and trigonometric functions. Roughly the 
+Break exponential terms into hyperbolic and trigonometric functions. Roughly the
 opposite in function to `exponentialize`
 
 ## Examples
 ```julia
 julia> demoivre(m\"exp(a + %i * b)\")
- 
+
                              a
                            %e  (%i sin(b) + cos(b))
 
 julia> exponentialize(ans)
- 
+
                          %i b     - %i b     %i b     - %i b
                     a  %e     + %e         %e     - %e
                   %e  (----------------- + -----------------)
                                2                   2
 
 julia> expand(ans)
- 
+
                                     %i b + a
                                   %e
 
 ```
-"""
-function demoivre{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("demoivre($mexpr)"))
-    convert(T, out)
-end
+""" demoivre
 
-"""
+@doc """
     exponentialize(expr)
 
 Express expression in terms of exponents as much as possible
@@ -374,21 +295,16 @@ Express expression in terms of exponents as much as possible
 ## Examples
 ```julia
 julia> exponentialize(m\"sin(x)\")
- 
+
                                    %i x     - %i x
                              %i (%e     - %e      )
                            - ----------------------
                                        2
 
 ```
-"""
-function exponentialize{T}(expr::T)
-    mexpr = MExpr(expr)
-    out = mcall(MExpr("exponentialize($mexpr)"))
-    convert(T, out)
-end
+""" exponentialize
 
-"""
+@doc """
     float(expr)
 
 Convert rational numbers into floating point numbers in expression.
@@ -396,17 +312,11 @@ Convert rational numbers into floating point numbers in expression.
 ## Examples
 ```julia
 julia> float(m\"1/3*x\")
- 
+
                              0.3333333333333333 x
 
 ```
-"""
-function float(expr::Union{Compat.String, Expr, MExpr})
-    T = typeof(expr)
-	mexpr = MExpr(expr)
-    out = mcall(MExpr("float($mexpr)"))
-    convert(T, out)
-end
+""" float
 
 """
     subst(a, b, expr)
