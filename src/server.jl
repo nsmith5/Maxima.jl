5cb0a717791d7aa6743ba2ce08467935d0b33c62
Sean Lake <odysseus9672@gmail.com>
2017-07-26 22:03:58 -0600

    Add the ability to pass command line arguments to Maxima (#30)
    * keyword argument `args` added `Maxima.Reset`, `Maxima.Load` and `MaximaSession`

--- src/server.jl
+++ src/server.jl
@@ -12,10 +12,10 @@ immutable MaximaSession <: Base.AbstractPipe
     output::Pipe
     process::Base.Process
 
-    function MaximaSession()
+    function MaximaSession(;args::String="")
         # If windows, executable is .bat
-        cmd = is_unix() ? `maxima --very-quiet` :
-            `maxima.bat --very-quiet`
+        cmd = is_unix() ? `maxima --very-quiet $args` :
+            `maxima.bat --very-quiet $args`
 
         # Setup pipes and maxima process
         input = Pipe()
